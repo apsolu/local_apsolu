61b7971d731bb367c42040566031bebab38dc035
Julien Boulen <julien.boulen@univ-rennes2.fr>
2017-07-04 14:12:09 +0200

    feat(version): augmente le numéro de version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2017042614;
+$plugin->version   = 2017070400;
 
 // Requires this Moodle version.
 $plugin->requires  = 2012112900;
