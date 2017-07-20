835e78efaa0cf5a8c8de882c04184b932def91fd
Julien Boulen <julien.boulen@univ-rennes2.fr>
2017-07-20 12:13:17 +0200

    feat(homepage): définis un focus javascript sur la page d'accueil lorsqu'elle est chargée avec une ancre

--- amd/src/homepage.js
+++ amd/src/homepage.js
@@ -3,6 +3,30 @@ define(["local_apsolu/lib_smoothscroll", "jquery"], function(smoothScroll, $) {
         initialise : function() {
             smoothScroll.init();
 
+            // Active le focus si une page est chargée avec un lien contenant une ancre.
+            if (window.location.hash) {
+
+                var menuid = '';
+                switch(window.location.hash) {
+                    case '#accueil':
+                        menuid = 'apsolu-home-a';
+                        break;
+                    case '#activites':
+                        menuid = 'apsolu-activities-a';
+                        break;
+                    case '#inscription':
+                        menuid = 'apsolu-signup-a';
+                        break;
+                }
+
+                if (menuid != '') {
+                    var menu = document.getElementById(menuid);
+                    if (menu) {
+                        menu.focus();
+                    }
+                }
+            }
+
             $('.apsolu-activities-description-div, .apsolu-slots-table').css('display', 'none');
             $('#apsolu-activities-content-legend-div').css('display', 'none');
             $('.apsolu-activities-list-h4').addClass('apsolu-activities-list-h4-link btn btn-default');
