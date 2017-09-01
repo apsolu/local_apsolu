0d1eb9145d02804e3d0847e5222d38a4e8befb68
Julien Boulen <julien.boulen@univ-rennes2.fr>
2017-09-01 18:15:33 +0200

    fix(version): incrémente le numéro de version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2017082201;
+$plugin->version   = 2017090100;
 
 // Requires this Moodle version.
 $plugin->requires  = 2012112900;
