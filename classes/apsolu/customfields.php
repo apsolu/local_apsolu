a5cdba9a1a6af888c692da93bb654916a044b9d7
Julien Boulen <julien.boulen@univ-rennes2.fr>
2018-10-08 18:24:03 +0200

    fix(core): ajoute une classe pour gérer les customfields

---
+++ classes/apsolu/customfields.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Fonctions pour le module apsolu.
+ *
+ * @package    local_apsolu
+ * @copyright  2018 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace UniversiteRennes2\Apsolu;
+
+class CustomFields {
+    public static function getCustomFields() {
+        global $DB;
+
+        $fields = array();
+
+        $sql = "SELECT * FROM {user_info_field} WHERE shortname LIKE 'apsolu%'";
+        foreach ($DB->get_records_sql($sql) as $field) {
+            $fields[$field->shortname] = $field;
+        }
+
+        return $fields;
+    }
+}
