65094c061efbe72e787278fd7df2bd22a54da923
Julien Boulen <julien.boulen@univ-rennes2.fr>
2019-01-18 12:58:02 +0100

    fix(webservices): supprime l'utilisation des fieldid codés en dur sur la méthode get_users

--- externallib.php
+++ externallib.php
@@ -74,15 +74,17 @@ class local_apsolu_webservices extends external_api {
             return $data;
         }
 
-        $sql = "SELECT DISTINCT u.id AS iduser, u.username, u.auth, u.firstname, u.lastname, uid1.data AS cardnumber, 'category', u.institution, uid2.data AS nosportcard".
+        $fields = $DB->get_records('user_info_field', $conditions = array(), $sort = '', $fields = 'shortname, id');
+
+        $sql = "SELECT DISTINCT u.id AS iduser, u.username, u.auth, u.firstname, u.lastname, IFNULL(uid1.data, '') AS cardnumber, 'category', u.institution, IFNULL(uid2.data, '') AS nosportcard".
             " FROM {user} u".
             " JOIN {user_enrolments} ue ON u.id = ue.userid AND status = 0". // Restreint le téléchargement des utilisateurs à ceux qui ont ou ont eu au moins une inscription active dans une activité. TODO: à supprimer à la fin du test.
-            " LEFT JOIN {user_info_data} uid1 ON u.id = uid1.userid AND uid1.fieldid = 16". // Numéro de carte VU.
-            " LEFT JOIN {user_info_data} uid2 ON u.id = uid2.userid AND uid2.fieldid = 12". // Carte sport.
+            " LEFT JOIN {user_info_data} uid1 ON u.id = uid1.userid AND uid1.fieldid = :apsoluidcardnumber". // Numéro de carte VU.
+            " LEFT JOIN {user_info_data} uid2 ON u.id = uid2.userid AND uid1.fieldid = :apsolucardpaid". // Carte sport.
             " WHERE u.timemodified >= :timemodified".
             " AND u.deleted = 0".
             " ORDER BY u.lastname, u.firstname";
-        foreach ($DB->get_records_sql($sql, array('timemodified' => $since)) as $record) {
+        foreach ($DB->get_records_sql($sql, array('timemodified' => $since, 'apsoluidcardnumber' => $fields['apsoluidcardnumber']->id, 'apsolucardpaid' => $fields['apsolucardpaid']->id)) as $record) {
             $user = new stdClass();
             $user->iduser = $record->iduser;
             $user->instuid = $record->auth.'|'.$record->username;
