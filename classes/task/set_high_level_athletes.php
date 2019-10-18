d63fbf6c093978d3ff484da661ccacb14defa0ed
Julien Boulen <julien.boulen@univ-rennes2.fr>
2019-10-18 17:10:37 +0200

    fix(task): corrige l'id du groupement pour les sportifs de haut niveau

--- classes/task/set_high_level_athletes.php
+++ classes/task/set_high_level_athletes.php
@@ -44,7 +44,7 @@ class set_high_level_athletes extends \core\task\scheduled_task {
         // TODO: carte muscu offerte.
 
         // TODO: faire une page pour configurer le groupe et le cours (menu déroulant + ids) à synchroniser.
-        $groupingid = 1;
+        $groupingid = 4;
         $courseid = 423;
 
         $field = $DB->get_record('user_info_field', array('shortname' => 'apsoluhighlevelathlete'));
