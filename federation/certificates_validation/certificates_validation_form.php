1ed25aca3ba76858b4f00ac031e3e9e639a2c81d
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-02-01 11:19:20 +0100

    fix(federation): modifie le champ sélectionné par défaut sur le formulaire de validation des certificats

--- federation/certificates_validation/certificates_validation_form.php
+++ federation/certificates_validation/certificates_validation_form.php
@@ -53,7 +53,8 @@ class local_apsolu_federation_certificates_validation extends moodleform {
 
         // État du certificat médical.
         $mform->addElement('select', 'medical_certificate_status', get_string('medical_certificate_status', 'local_apsolu'), $certificates);
-        $mform->setType('medical', PARAM_INT);
+        $mform->setType('medical_certificate_status', PARAM_INT);
+        $mform->setDefault('medical_certificate_status', APSOLU_SELECT_NO);
 
         // Submit buttons.
         $attributes = array('class' => 'btn btn-default');
