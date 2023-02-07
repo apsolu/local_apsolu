07157cbcd0403ca8f04124c7b24a392831a391e8
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-02-07 15:52:00 +0100

    feat(federation): réaffiche toutes les informations de l'étudiant sur l'onglet récapitulatif une fois le numéro de licence attribué

--- federation/adhesion/summary.php
+++ federation/adhesion/summary.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use local_apsolu\core\federation\activity as Activity;
+use local_apsolu\core\federation\adhesion as Adhesion;
 use UniversiteRennes2\Apsolu\Payment;
 
 defined('MOODLE_INTERNAL') || die();
@@ -147,6 +149,123 @@ if (empty($adhesion->federationnumber) === true) {
     // Affiche le formulaire.
     $mform->display();
 } else {
-    $label = get_string('a_license_number_has_been_assigned_to_you_by_the_federation', 'local_apsolu');
-    echo html_writer::div($label, 'alert alert-success');
+    $getconfig = get_config('local_apsolu');
+
+    $data = new stdClass();
+    $data->fields = [];
+    $data->fields[] = ['label' => get_string('federation_number', 'local_apsolu'), 'value' => $adhesion->federationnumber];
+    $data->fields[] = ['label' => get_string('lastname'), 'value' => $USER->lastname];
+    $data->fields[] = ['label' => get_string('firstname'), 'value' => $USER->firstname];
+    $data->fields[] = ['label' => get_string('email'), 'value' => $USER->email];
+    $data->fields[] = ['label' => get_string('sex', 'local_apsolu'), 'value' => $adhesion->sex];
+    $data->fields[] = ['label' => get_string('birthday', 'local_apsolu'), 'value' => userdate($adhesion->birthday, get_string('strftimedate'))];
+    $data->fields[] = ['label' => get_string('address1', 'local_apsolu'), 'value' => $adhesion->address1];
+    $data->fields[] = ['label' => get_string('address2', 'local_apsolu'), 'value' => $adhesion->address2];
+    $data->fields[] = ['label' => get_string('postal_code', 'local_apsolu'), 'value' => $adhesion->postalcode];
+    $data->fields[] = ['label' => get_string('city'), 'value' => $adhesion->city];
+    $data->fields[] = ['label' => get_string('phone', 'local_apsolu'), 'value' => $adhesion->phone];
+
+    // On récupère la discipline.
+    $value = '';
+    $disciplines = Adhesion::get_disciplines();
+    if (isset($disciplines[$adhesion->disciplineid]) === true) {
+        $value = $disciplines[$adhesion->disciplineid];
+    }
+    $data->fields[] = ['label' => get_string('discipline', 'local_apsolu'), 'value' => $value];
+
+    // On récupère le champ autre fédération.
+    if (empty($getconfig->otherfederation) === false) {
+        $data->fields[] = ['label' => get_string('other_federation', 'local_apsolu'), 'value' => $adhesion->otherfederation];
+    }
+
+    // On récupère le champ sport principal.
+    $value = '';
+    $activities = Activity::get_activity_data();
+    if (isset($activities[$adhesion->mainsport]) === true) {
+        $value = $activities[$adhesion->mainsport]['name'];
+    }
+    $data->fields[] = ['label' => get_string('main_sport', 'local_apsolu'), 'value' => $value];
+
+    // On récupère la liste des champs oui/non.
+    $yesnofields = array();
+    $yesnofields['sportlicense'] = get_string('sport_license', 'local_apsolu');
+    $yesnofields['managerlicense'] = get_string('manager_license', 'local_apsolu');
+    $yesnofields['managerlicensetype'] = get_string('manager_license_type', 'local_apsolu');
+    $yesnofields['refereelicense'] = get_string('referee_license', 'local_apsolu');
+    $yesnofields['starlicense'] = get_string('star_license', 'local_apsolu');
+    $yesnofields['insurance'] = get_string('insurance', 'local_apsolu');
+    $yesnofields['instagram'] = get_string('instagram', 'local_apsolu');
+    $yesnofields['usepersonaldata'] = get_string('use_personal_data', 'local_apsolu');
+    foreach ($yesnofields as $field => $label) {
+        $visibilitystrname = sprintf('%s_field_visibility', $field);
+        if (empty($getconfig->$visibilitystrname) === true) {
+            continue;
+        }
+
+        if ($field === 'managerlicensetype') {
+            if (empty($adhesion->managerlicense) === true || empty($getconfig->managerlicense) === true) {
+                // N'affiche pas la licence dirigeante si elle est positionnée à Non et/ou non visible.
+                continue;
+            }
+            $value = get_string('not_student', 'local_apsolu');
+        } else {
+            $value = get_string('no');
+        }
+
+        if (empty($adhesion->{$field}) === false) {
+            if ($field === 'managerlicensetype') {
+                $value = get_string('student');
+            } else {
+                $value = get_string('yes');
+            }
+        }
+
+        $data->fields[] = ['label' => $label, 'value' => $value];
+    }
+
+    // On récupère la liste des sports autorisés par le certificat médical.
+    $fieldname = 'sport';
+    $label = get_string('activity_without_constraint', 'local_apsolu');
+    if ($adhesion->sport1 === Adhesion::SPORT_NONE) {
+        $fieldname = 'constraintsport';
+        $label = get_string('activity_with_specific_constraints', 'local_apsolu');
+    }
+
+    for ($i = 1; $i <= 5; $i++) {
+        $field = $fieldname.$i;
+        if ($adhesion->$field === Adhesion::SPORT_NONE) {
+            break;
+        }
+
+        if (isset($activities[$adhesion->$field]['name']) === false) {
+            break;
+        }
+
+        $data->fields[] = ['label' => $label, 'value' => $activities[$adhesion->$field]['name']];
+    }
+
+    // On récupère la date du certificat médical.
+    if (empty($adhesion->medicalcertificatedate) === false) {
+        $value = userdate($adhesion->medicalcertificatedate, get_string('strftimedate'));
+        $data->fields[] = ['label' => get_string('medical_certificate_date', 'local_apsolu'), 'value' => $value];
+    }
+
+    // On récupère les certificats.
+    $fs = get_file_storage();
+    $context = context_course::instance($courseid, MUST_EXIST);
+    list($component, $filearea, $itemid) = array('local_apsolu', 'medicalcertificate', $USER->id);
+    $sort = 'itemid, filepath, filename';
+    $files = $fs->get_area_files($context->id, $component, $filearea, $itemid, $sort, $includedirs = false);
+
+    $items = array();
+    foreach ($files as $file) {
+        $url = moodle_url::make_pluginfile_url($context->id, $component, $filearea, $itemid, '/', $file->get_filename(), $forcedownload = false, $includetoken = false);
+        $items[] = html_writer::link($url, $file->get_filename());
+    }
+
+    if (isset($items[0]) === true) {
+        $data->fields[] = ['label' => get_string('medical_certificate', 'local_apsolu'), 'htmlvalue' => html_writer::alist($items)];
+    }
+
+    echo $OUTPUT->render_from_template('local_apsolu/federation_adhesion_summary', $data);
 }
