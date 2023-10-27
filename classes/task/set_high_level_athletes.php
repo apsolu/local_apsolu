1f2769dc2bdb2c40d65a1733e44c7055c700b245
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-10-27 11:33:40 +0200

    feat(shn): offre l'accès à la salle de musculation pour les sportifs de haut niveau à Rennes

--- classes/task/set_high_level_athletes.php
+++ classes/task/set_high_level_athletes.php
@@ -24,7 +24,9 @@
 
 namespace local_apsolu\task;
 
-use UniversiteRennes2\Apsolu as apsolu;
+use context_system;
+use stdClass;
+use UniversiteRennes2\Apsolu\Payment;
 
 /**
  * Classe représentant la tâche permettant d'alimenter le témoin sportif de haut niveau.
@@ -58,8 +60,12 @@ class set_high_level_athletes extends \core\task\scheduled_task {
         }
 
         // TODO: faire une page pour configurer le groupe et le cours (menu déroulant + ids) à synchroniser.
+        require_once($CFG->dirroot . '/local/apsolu/classes/apsolu/payment.php');
+
         $groupingid = 4;
         $courseid = 423;
+
+        $workoutcourseid = 250;
 
         $field = $DB->get_record('user_info_field', array('shortname' => 'apsoluhighlevelathlete'));
         if ($field === false) {
@@ -73,12 +79,15 @@ class set_high_level_athletes extends \core\task\scheduled_task {
             return false;
         }
 
+        $cards = $DB->get_records('apsolu_payments_cards');
+
         $sql = "SELECT DISTINCT gm.userid".
             " FROM {groups_members} gm".
             " JOIN {groupings_groups} gg ON gg.groupid = gm.groupid".
             " WHERE gg.groupingid = :groupingid";
         $members = $DB->get_records_sql($sql, array('groupingid' => $grouping->id));
         foreach ($members as $member) {
+            // Positionne le témoin sportifs de haut niveau dans le profil de l'utilisateur.
             $data = new \stdClass();
             $data->userid = $member->userid;
             $data->fieldid = $field->id;
@@ -96,8 +105,73 @@ class set_high_level_athletes extends \core\task\scheduled_task {
                 $DB->insert_record('user_info_data', $data);
                 mtrace("\t insert ".$field->shortname." : userid=".$data->userid." data=".$data->data);
             }
+
+            // Offre l'accès à la salle de musculation aux sportifs de haut niveau.
+            $items = [];
+            foreach (Payment::get_user_cards_status_per_course($workoutcourseid, $member->userid) as $card) {
+                if ($card->status != Payment::DUE) {
+                    continue;
+                }
+
+                if (isset($cards[$card->id]) === false) {
+                    continue;
+                }
+
+                $centerid = $cards[$card->id]->centerid;
+                if (isset($items[$centerid]) === false) {
+                    $items[$centerid] = [];
+                }
+
+                $items[$centerid][] = $card->id;
+            }
+
+            foreach ($items as $centerid => $cardsid) {
+                $payment = new stdClass();
+                $payment->id = 0;
+                $payment->method = 'coins';
+                $payment->source = 'apsolu';
+                $payment->amount = 0;
+                $payment->status = Payment::GIFT;
+                $payment->timepaid = '';
+                $payment->timecreated = strftime('%FT%T');
+                $payment->timemodified = $payment->timecreated;
+                $payment->timepaid = $payment->timecreated;
+                $payment->userid = $member->userid;
+                $payment->paymentcenterid = $centerid;
+
+                try {
+                    $transaction = $DB->start_delegated_transaction();
+
+                    $payment->id = $DB->insert_record('apsolu_payments', $payment);
+                    mtrace("\t insert apsolu_payments: userid=".$member->userid." status=gift, centerid=".$centerid);
+
+                    foreach ($cardsid as $cardid) {
+                        $item = new stdClass();
+                        $item->paymentid = $payment->id;
+                        $item->cardid = $cardid;
+
+                        $DB->insert_record('apsolu_payments_items', $item);
+                        mtrace("\t insert apsolu_payments_items: paymentid=".$payment->id.", cardid=".$cardid);
+                    }
+
+                    $event = \local_apsolu\event\update_user_payment::create(array(
+                        'relateduserid' => $member->userid,
+                        'context' => context_system::instance(),
+                        'other' => json_encode(array('payment' => $payment, 'items' => $cardsid)),
+                    ));
+                    $event->trigger();
+
+                    $success = true;
+
+                    $transaction->allow_commit();
+                } catch (Exception $exception) {
+                    $success = false;
+                    $transaction->rollback($exception);
+                }
+            }
         }
 
+        // Supprime le témoin sportifs de haut niveau.
         $datas = $DB->get_records('user_info_data', array('fieldid' => $field->id));
         foreach ($datas as $data) {
             if ($data->data !== '1') {
