bd77ae24e2d00136e7243c8e87eff60aa2351c8a
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-10-31 10:24:58 +0100

    style(payment): corrige le coding style du formulaire de saisie de l'adresse postale du porteur de la carte de paiement

--- payment/address_form.php
+++ payment/address_form.php
@@ -55,7 +55,8 @@ class local_apsolu_payment_address_form extends moodleform {
 
         $mform->addElement('text', 'lastname', get_string('cardholder_lastname', 'local_apsolu'), $attributes);
         $mform->setType('lastname', PARAM_TEXT);
-        $mform->addRule('lastname', get_string('maximumchars', '', paybox::MAX_LENGTH_LASTNAME), 'maxlength', paybox::MAX_LENGTH_LASTNAME, 'server');
+        $errormessage = get_string('maximumchars', '', paybox::MAX_LENGTH_LASTNAME);
+        $mform->addRule('lastname', $errormessage, 'maxlength', paybox::MAX_LENGTH_LASTNAME, 'server');
         $mform->addRule('lastname', get_string('required'), 'required', null, 'client');
 
         // Firstname field.
@@ -63,7 +64,8 @@ class local_apsolu_payment_address_form extends moodleform {
 
         $mform->addElement('text', 'firstname', get_string('cardholder_firstname', 'local_apsolu'), $attributes);
         $mform->setType('firstname', PARAM_TEXT);
-        $mform->addRule('firstname', get_string('maximumchars', '', paybox::MAX_LENGTH_FIRSTNAME), 'maxlength', paybox::MAX_LENGTH_FIRSTNAME, 'server');
+        $errormessage = get_string('maximumchars', '', paybox::MAX_LENGTH_FIRSTNAME);
+        $mform->addRule('firstname', $errormessage, 'maxlength', paybox::MAX_LENGTH_FIRSTNAME, 'server');
         $mform->addRule('firstname', get_string('required'), 'required', null, 'client');
 
         // Address1 field.
@@ -71,21 +73,25 @@ class local_apsolu_payment_address_form extends moodleform {
 
         $mform->addElement('text', 'address1', get_string('cardholder_postal_address', 'local_apsolu'), $attributes);
         $mform->setType('address1', PARAM_TEXT);
-        $mform->addRule('address1', get_string('maximumchars', '', paybox::MAX_LENGTH_ADDRESS1), 'maxlength', paybox::MAX_LENGTH_ADDRESS1, 'server');
+        $errormessage = get_string('maximumchars', '', paybox::MAX_LENGTH_ADDRESS1);
+        $mform->addRule('address1', $errormessage, 'maxlength', paybox::MAX_LENGTH_ADDRESS1, 'server');
         $mform->addRule('address1', get_string('required'), 'required', null, 'client');
 
         // Address2 field.
         $attributes['maxlength'] = paybox::MAX_LENGTH_ADDRESS2;
 
-        $mform->addElement('text', 'address2', get_string('cardholder_additional_postal_address_optional', 'local_apsolu'), $attributes);
-        $mform->addRule('address2', get_string('maximumchars', '', paybox::MAX_LENGTH_ADDRESS2), 'maxlength', paybox::MAX_LENGTH_ADDRESS2, 'server');
+        $label = get_string('cardholder_additional_postal_address_optional', 'local_apsolu');
+        $mform->addElement('text', 'address2', $label, $attributes);
+        $errormessage = get_string('maximumchars', '', paybox::MAX_LENGTH_ADDRESS2);
+        $mform->addRule('address2', $errormessage, 'maxlength', paybox::MAX_LENGTH_ADDRESS2, 'server');
         $mform->setType('address2', PARAM_TEXT);
 
         // Zipcode field.
         $attributes['maxlength'] = paybox::MAX_LENGTH_ZIPCODE;
 
         $mform->addElement('text', 'zipcode', get_string('cardholder_zipcode', 'local_apsolu'), $attributes);
-        $mform->addRule('zipcode', get_string('maximumchars', '', paybox::MAX_LENGTH_ZIPCODE), 'maxlength', paybox::MAX_LENGTH_ZIPCODE, 'server');
+        $errormessage = get_string('maximumchars', '', paybox::MAX_LENGTH_ZIPCODE);
+        $mform->addRule('zipcode', $errormessage, 'maxlength', paybox::MAX_LENGTH_ZIPCODE, 'server');
         $mform->setType('zipcode', PARAM_TEXT);
         $mform->addRule('zipcode', get_string('required'), 'required', null, 'client');
 
@@ -94,12 +100,14 @@ class local_apsolu_payment_address_form extends moodleform {
 
         $mform->addElement('text', 'city', get_string('cardholder_city', 'local_apsolu'), $attributes);
         $mform->setType('city', PARAM_TEXT);
-        $mform->addRule('city', get_string('maximumchars', '', paybox::MAX_LENGTH_CITY), 'maxlength', paybox::MAX_LENGTH_CITY, 'server');
+        $errormessage = get_string('maximumchars', '', paybox::MAX_LENGTH_CITY);
+        $mform->addRule('city', $errormessage, 'maxlength', paybox::MAX_LENGTH_CITY, 'server');
         $mform->addRule('city', get_string('required'), 'required', null, 'client');
 
         // CountryCode field.
         $multiple = ['multiple' => false];
-        $mform->addElement('autocomplete', 'countrycode', get_string('cardholder_country', 'local_apsolu'), country::get_iso_3166_3(), $multiple);
+        $label = get_string('cardholder_country', 'local_apsolu');
+        $mform->addElement('autocomplete', 'countrycode', $label, country::get_iso_3166_3(), $multiple);
         $mform->setType('countrycode', PARAM_INT);
         $mform->addRule('countrycode', get_string('required'), 'required', null, 'client');
 
