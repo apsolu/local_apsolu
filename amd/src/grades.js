e206c049c558d0752fb492a686c6a85a6f269c1e
Julien Boulen <julien.boulen@univ-rennes2.fr>
2024-02-08 17:16:56 +0100

    style(javascript): corrige le coding style du script grades.js

--- amd/src/grades.js
+++ amd/src/grades.js
@@ -1,34 +1,33 @@
 define(['jquery'], function($) {
     return {
-        initialise : function(){
-
+        initialise: function() {
             // Gère les modifications de la checkbox.
-            $("#apsolu-form-grades input[type='checkbox']").change(function(){
+            $("#apsolu-form-grades input[type='checkbox']").change(function() {
                 var name = $(this).attr("name");
                 var id = name.substring(3);
 
                 if ($(this).prop("checked")) {
                     if (name.substring(0, 3) == "abj") {
-                        $("input[name='grades"+id+"']").val('ABJ');
-                        $("input[name='abi"+id+"']").prop('checked', false);
+                        $("input[name='grades" + id + "']").val('ABJ');
+                        $("input[name='abi" + id + "']").prop('checked', false);
                     } else {
-                        $("input[name='grades"+id+"']").val('ABI');
-                        $("input[name='abj"+id+"']").prop('checked', false);
+                        $("input[name='grades" + id + "']").val('ABI');
+                        $("input[name='abj" + id + "']").prop('checked', false);
                     }
-                    $("input[name='grades"+id+"']").prop('readonly', true);
+                    $("input[name='grades" + id + "']").prop('readonly', true);
                 } else {
-                    $("input[name='grades"+id+"']").prop('readonly', false);
-                    $("input[name='grades"+id+"']").val('');
+                    $("input[name='grades" + id + "']").prop('readonly', false);
+                    $("input[name='grades" + id + "']").val('');
                 }
             });
 
             // Gère les modifications de la checkbox.
-            $("#apsolu-form-grades input[type='checkbox']").each(function(){
+            $("#apsolu-form-grades input[type='checkbox']").each(function() {
                 var name = $(this).attr("name");
                 var id = name.substring(3);
 
                 if ($(this).prop("checked")) {
-                    $("input[name='grades"+id+"']").prop('readonly', true);
+                    $("input[name='grades" + id + "']").prop('readonly', true);
                 }
             });
         }
