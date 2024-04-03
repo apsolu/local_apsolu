efc0cc8a0aa0c12dc163e40e311c6d47389a8138
Julien Boulen <julien.boulen@univ-rennes2.fr>
2024-04-03 11:37:29 +0200

    fix(javascript): corrige le fonctionnement du stickyHeaders de TableSorter

--- amd/src/sort.js
+++ amd/src/sort.js
@@ -1,6 +1,11 @@
 define(["jquery", "local_apsolu/jquery.tablesorter"], function($) {
     return {
         initialise: function(options = {}) {
+            if (options.hasOwnProperty("widgets") && options.widgets.includes("stickyHeaders")) {
+                // Modifie le style de <body> afin de faire fonctionner le stickyHeaders de TableSorter.
+                document.body.style.height = 'auto';
+            }
+
             $(".table-sortable").tablesorter(options);
         }
     };
