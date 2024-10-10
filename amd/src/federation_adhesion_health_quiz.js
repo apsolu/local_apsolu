18a0793b2189953f7946f38255db6ac1c461966f
Julien Boulen <julien.boulen@univ-rennes2.fr>
2024-10-10 16:00:55 +0200

    feat(federation): met à jour le questionnaire de santé

--- amd/src/federation_adhesion_health_quiz.js
+++ amd/src/federation_adhesion_health_quiz.js
@@ -1,21 +1,23 @@
 define([], function() {
     return {
-        initialise : function() {
+        initialise: function() {
             // Récupère tous les champs radio.
-            var inputs = document.querySelectorAll('#healthquizquestionsform input[type=radio]');
-            for (var i = 0; i < inputs.length; i++) {
+            let inputs = document.querySelectorAll('#healthquizquestionsform input[type=radio]');
+            for (let i = 0; i < inputs.length; i++) {
                 // Ajoute un écouteur sur chaque bouton radio.
-                inputs[i].addEventListener('change', function(evnt) {
+                inputs[i].addEventListener('change', function() {
                     // Récupère la liste des boutons radio cochés.
-                    var inputs = document.querySelectorAll('#healthquizquestionsform input[type=radio]:checked');
+                    let allinputs = document.querySelectorAll('#healthquizquestionsform input[type=radio]');
+                    let inputs = document.querySelectorAll('#healthquizquestionsform input[type=radio]:checked');
 
                     // Si tous les boutons radio sont cochés, on calcule le score.
-                    if (inputs.length === 9) {
+                    // La variable allinputs.length contient les checkboxes oui/non. Donc, on divise par 2.
+                    if (inputs.length === (allinputs.length / 2)) {
                         // On initialise le score à 0.
-                        var healthquizvalue = document.getElementById('healthquizvalue');
+                        let healthquizvalue = document.getElementById('healthquizvalue');
                         healthquizvalue.value = 0;
 
-                        for (var i = 0; i < inputs.length; i++) {
+                        for (let i = 0; i < inputs.length; i++) {
                             if (inputs[i].value === '0') {
                                 continue;
                             }
@@ -26,11 +28,11 @@ define([], function() {
                         }
 
                         // Déverrouille le bouton d'envoi du formulaire.
-                        var submit = document.getElementById('healthquizsubmit');
+                        let submit = document.getElementById('healthquizsubmit');
                         submit.removeAttribute('disabled');
                     }
                 });
             }
         }
-    }
+    };
 });
--- classes/core/federation/questionnaire.php
+++ classes/core/federation/questionnaire.php
@@ -46,20 +46,36 @@ class questionnaire {
         $category = new stdClass();
         $category->name = 'Durant les 12 derniers mois';
         $category->questions = [];
-        $category->questions[] = (object) ['id' => 'q1', 'label' => 'Un membre de votre famille est-il décédé subitement d’une cause cardiaque ou inexpliquée ?'];
-        $category->questions[] = (object) ['id' => 'q2', 'label' => 'Avez-vous ressenti une douleur dans la poitrine, des palpitations, un essoufflement inhabituel ou un malaise ?'];
-        $category->questions[] = (object) ['id' => 'q3', 'label' => 'Avez-vous eu un épisode de respiration sifflante (asthme) ?'];
-        $category->questions[] = (object) ['id' => 'q4', 'label' => 'Avez-vous eu une perte de connaissance ?'];
-        $category->questions[] = (object) ['id' => 'q5', 'label' => 'Si vous avez arrêté le sport pendant 30 jours ou plus pour des raisons de santé, avez-vous repris sans l’accord d’un médecin ?'];
-        $category->questions[] = (object) ['id' => 'q6', 'label' => 'Avez-vous débuté un traitement médical de longue durée (hors contraception et désensibilisation aux allergies) ?'];
+        $category->questions[] = (object) ['id' => 'q1', 'label' => 'Un membre de votre famille est-il décédé subitement
+            d’une cause cardiaque ou inexpliquée ?'];
+        $category->questions[] = (object) ['id' => 'q2', 'label' => 'Avez-vous ressenti une douleur dans la poitrine,
+            des palpitations, un essoufflement inhabituel ou un malaise ?'];
+        $category->questions[] = (object) ['id' => 'q3', 'label' => 'Avez-vous ressenti des douleurs au niveau de la colonne
+            vertébrale (cervicales, dorsales, lombaires) ?'];
+        $category->questions[] = (object) ['id' => 'q4', 'label' => 'Avez-vous eu un épisode de respiration sifflante (asthme) ?'];
+        $category->questions[] = (object) ['id' => 'q5', 'label' => 'Avez-vous eu une perte de connaissance ?'];
+        $category->questions[] = (object) ['id' => 'q6', 'label' => 'Si vous avez arrêté le sport pendant 30 jours ou plus
+            pour des raisons de santé, avez-vous repris sans l’accord d’un médecin ?'];
+        $category->questions[] = (object) ['id' => 'q7', 'label' => 'Avez-vous été arrêté pour suspicion de commotion cérébrale ou
+            pensez-vous avoir subi une commotion cérébrale ?'];
+        $category->questions[] = (object) ['id' => 'q8', 'label' => 'Avez-vous débuté un traitement médical de longue durée
+            (hors contraception et désensibilisation aux allergies) ?'];
         $questions[] = $category;
 
         $category = new stdClass();
         $category->name = 'À ce jour';
         $category->questions = [];
-        $category->questions[] = (object) ['id' => 'q7', 'label' => 'Ressentez-vous une douleur, un manque de force ou une raideur suite à un problème osseux, articulaire ou musculaire (fracture, entorse, luxation, déchirure, tendinite, etc...) survenu durant les 12 derniers mois ?'];
-        $category->questions[] = (object) ['id' => 'q8', 'label' => 'Votre pratique sportive est-elle interrompue pour des raisons de santé ?'];
-        $category->questions[] = (object) ['id' => 'q9', 'label' => 'Pensez-vous avoir besoin d’un avis médical pour poursuivre votre pratique sportive ?'];
+        $category->questions[] = (object) ['id' => 'q9', 'label' => 'Un membre de votre famille a-t-il présenté une maladie
+            cardiaque grave avant l’âge de 50 ans ? (Arrêt cardiaque, infarctus, stent, pontage, pose déﬁbrillateur, pacemaker).'];
+        $category->questions[] = (object) ['id' => 'q10', 'label' => 'Présentez-vous une myopie supérieure à 6 dioptries, une
+            fragilisation acquise du globe oculaire ou un œil unique ?'];
+        $category->questions[] = (object) ['id' => 'q11', 'label' => 'Ressentez-vous une douleur, un manque de force ou une raideur
+            suite à un problème osseux, articulaire ou musculaire (fracture, entorse, luxation, déchirure, tendinite, etc...)
+            survenu durant les 12 derniers mois ?'];
+        $category->questions[] = (object) ['id' => 'q12', 'label' => 'Votre pratique sportive est-elle interrompue
+            pour des raisons de santé ?'];
+        $category->questions[] = (object) ['id' => 'q13', 'label' => 'Pensez-vous avoir besoin d’un avis médical pour poursuivre
+            votre pratique sportive ?'];
         $questions[] = $category;
 
         return $questions;
