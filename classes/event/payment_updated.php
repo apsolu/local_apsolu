67c7ed618d50ba3d92da53578b8c3ab0d8a9e6ef
Julien Boulen <julien.boulen@univ-rennes2.fr>
2026-02-05 12:28:39 +0100

    feat(payment): ajoute deux nouveaux évènements \local_apsolu\event\payment_created et \local_apsolu\event\payment_updated

---
+++ classes/event/payment_updated.php
@@ -0,0 +1,75 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_apsolu\event;
+
+use moodle_url;
+
+/**
+ * Enregistre la mise à jour d'un paiement.
+ *
+ * @package    local_apsolu
+ * @copyright  2026 Université Rennes 2
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class payment_updated extends \core\event\base {
+    /**
+     * Initialise l'évènement.
+     *
+     * @return void
+     */
+    protected function init() {
+        // Values: c (create), r (read), u (update) or d (delete).
+        $this->data['crud'] = 'u';
+
+        // Values: LEVEL_TEACHING, LEVEL_PARTICIPATING or LEVEL_OTHER.
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+
+        $this->data['objecttable'] = 'apsolu_payments';
+    }
+
+    /**
+     * Retourne le nom de l'évènement.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('payment_updated', 'local_apsolu');
+    }
+
+    /**
+     * Retourne la description de l'évènement.
+     *
+     * @return string
+     */
+    public function get_description() {
+        if (isset($this->other->items) === false || is_array($this->other->items) === false) {
+            $this->other->items = [];
+        }
+
+        return 'The user #' . $this->userid . ' updated the payment #' . $this->objectid .
+            ' for the user #' . $this->relateduserid . '(cards: ' . implode(', ', $this->other->items) . ').';
+    }
+
+    /**
+     * Retourne l'url liée à l'évènement.
+     *
+     * @return moodle_url
+     */
+    public function get_url() {
+        return new moodle_url('/local/apsolu/payment/admin.php', ['tab' => 'payments', 'userid' => $this->relateduserid]);
+    }
+}
--- lang/en/local_apsolu.php
+++ lang/en/local_apsolu.php
@@ -686,6 +686,7 @@ $string['pay_with_my_pass_sport'] = 'Payer avec mon Pass Sport';
 $string['payment_approved'] = 'Paiment confirmé';
 $string['payment_center'] = 'Centre de paiement';
 $string['payment_center_help'] = 'Champ libre qui sera affiché sur la page de paiement des utilisateurs.';
+$string['payment_created'] = 'Paiment créé';
 $string['payment_prefix_help'] = 'Préfixe affiché avant le numéro de commande dans le backoffice paybox. Cela peut être utile si votre abonnement Paybox est utilisé par plusieurs applications.';
 $string['payment_prefix'] = 'Préfixe de paiement';
 $string['payments_extraction'] = 'Extraction des paiements';
