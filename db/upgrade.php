a36ff0b5f4ae771ab2e95c8dd42f0c7d1a26a9ca
Julien Boulen <julien.boulen@univ-rennes2.fr>
2026-08-31 14:11:03 +0200

    feat(core): publie la version 5.2.2

--- db/upgrade.php
+++ db/upgrade.php
@@ -25,8 +25,12 @@
 // phpcs:disable moodle.Files.LineLength.MaxExceeded
 // phpcs:disable moodle.Files.LineLength.TooLong
 
+use core_customfield\api as CustomfieldAPI;
+use core_customfield\data_controller;
 use local_apsolu\core\attendancesession;
 use local_apsolu\core\course;
+use local_apsolu\core\coursetype;
+use local_apsolu\core\customfields;
 use local_apsolu\core\federation\activity;
 use local_apsolu\core\federation\adhesion;
 use local_apsolu\core\federation\course as FederationCourse;
@@ -34,6 +38,8 @@ use local_apsolu\core\gradebook;
 use local_apsolu\core\messaging;
 use local_apsolu\core\municipality;
 use local_apsolu\core\reset;
+use local_apsolu\customfields\course as CustomfieldsCourse;
+use local_apsolu\payment\method;
 
 /**
  * Procédure de mise à jour.
@@ -2249,6 +2255,282 @@ function xmldb_local_apsolu_upgrade($oldversion = 0) {
 
         // Ajoute la configuration nécessaire pour la réinitisalisation des espaces cours et les permissions associées.
         reset::init_config();
+    }
+
+    $version = 2026083100;
+    if ($oldversion < $version) {
+        // Initialise les variables pour les méthodes de paiement.
+        method::init_config();
+
+        // Ajoute un champ "timelicensed" dans la table "apsolu_federation_adhesions".
+        $table = new xmldb_table('apsolu_federation_adhesions');
+        $field = new xmldb_field('timelicensed', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, 0, 'timemodified');
+
+        if ($dbman->field_exists($table, $field) === false) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Génère les tables pour la gestion des formats de cours.
+        $table = new xmldb_table('apsolu_courses_types');
+        if ($dbman->table_exists($table) === false) {
+            $previous = null;
+            $nodefault = null;
+            $nosequence = null;
+            $notnull = XMLDB_NOTNULL;
+
+            // Adding fields.
+            $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, XMLDB_SEQUENCE, $nodefault, $previous);
+            $table->add_field('shortname', XMLDB_TYPE_CHAR, '255', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault, $previous);
+            $table->add_field('name', XMLDB_TYPE_CHAR, '255', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault, $previous);
+            $table->add_field('fullnametemplate', XMLDB_TYPE_CHAR, '255', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault, $previous);
+            $table->add_field('color', XMLDB_TYPE_CHAR, '255', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault, $previous);
+            $table->add_field('sortorder', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, $nosequence, $default = 0, $previous);
+
+            // Adding key.
+            $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+            // Adding indexes.
+            $table->add_index('shortname', XMLDB_INDEX_UNIQUE, ['shortname']);
+            $table->add_index('sortorder', XMLDB_INDEX_NOTUNIQUE, ['sortorder']);
+
+            // Create table.
+            $dbman->create_table($table);
+
+            // Initialise les champs personnalisés de cours.
+            CustomfieldsCourse::initialize_course_customfields();
+        }
+
+        $table = new xmldb_table('apsolu_courses_fields');
+        if ($dbman->table_exists($table) === false) {
+            $previous = null;
+            $nodefault = null;
+            $nosequence = null;
+            $notnull = XMLDB_NOTNULL;
+
+            // Adding fields.
+            $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, XMLDB_SEQUENCE, $nodefault, $previous);
+            $table->add_field('coursetypeid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault, $previous);
+            $table->add_field('customfieldid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault, $previous);
+            $table->add_field('showinadministration', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault);
+            $table->add_field('showonpublicpages', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, $notnull, $nosequence, $nodefault);
+
+            // Adding key.
+            $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+            // Adding indexes.
+            $table->add_index('unique', XMLDB_INDEX_UNIQUE, ['coursetypeid, customfieldid']);
+
+            // Create table.
+            $dbman->create_table($table);
+        }
+
+        $params = ['name' => 'APSOLU', 'component' => 'core_course', 'area' => 'course', 'itemid' => 0];
+        $category = $DB->get_record('customfield_category', $params, '*', MUST_EXIST);
+
+        $fields = [];
+        foreach (CustomfieldAPI::get_categories_with_fields('core_course', 'course', 0) as $customcategory) {
+            if ($customcategory->get('id') != $category->id) {
+                continue;
+            }
+
+            foreach ($customcategory->get_fields() as $field) {
+                $fields[$field->get('shortname')] = $field;
+            }
+
+            break;
+        }
+
+        $table = new xmldb_table('apsolu_courses');
+        if ($dbman->table_exists($table) === true) {
+            // Initialise un type de format "cours".
+            CourseType::initialize_course_type();
+            $coursetype = $DB->get_record('apsolu_courses_types', ['shortname' => strtolower(get_string('course'))], '*', MUST_EXIST);
+
+            // Migre l'ancienne table mdl_apsolu_courses.
+            $courses = $DB->get_records('course', $conditions = null, $sort = '', 'id, fullname, category');
+            foreach ($DB->get_records('apsolu_courses') as $course) {
+                if (isset($courses[$course->id]) === false) {
+                    // Note: cette situation ne devrait pas exister.
+                    continue;
+                }
+
+                if (defined('CLI_SCRIPT') === true) {
+                    mtrace('Migre ' . $courses[$course->id]->fullname);
+                }
+
+                $coursecontext = context_course::instance($course->id);
+                foreach ($fields as $field) {
+                    $shortname = $field->get('shortname');
+                    $fieldnameform = 'customfield_' . $shortname;
+
+                    $params = (object) ['instanceid' => $course->id, 'contextid' => $coursecontext->id];
+                    $data = data_controller::create(0, $params, $field);
+
+                    $value = null;
+                    switch ($shortname) {
+                        case 'type':
+                            $value = $coursetype->id;
+                            break;
+                        case 'category':
+                            $value = ['categoryid' => $courses[$course->id]->category, 'additionalstr' => trim($course->event)];
+                            break;
+                        case 'location':
+                        case 'period':
+                        case 'skill':
+                            $key = $shortname . 'id';
+                            $value = $course->{$key};
+                            break;
+                        case 'weekday':
+                            $value = $course->numweekday;
+                            break;
+                        case 'timerange':
+                            $value = [];
+                            foreach (['start', 'end'] as $lapse) {
+                                $attribute = sprintf('%stime', $lapse);
+                                $raw = explode(':', $course->{$attribute});
+
+                                $value[$lapse] = [];
+                                $value[$lapse]['hour'] = $raw[0];
+                                $value[$lapse]['minute'] = $raw[1];
+                            }
+                            break;
+                        case 'daterange':
+                            $value = ['start' => 0, 'end' => 0];
+                            break;
+                        case 'federation':
+                        case 'on_homepage':
+                            if (empty($course->{$shortname}) === false) {
+                                $value = 1; // Oui.
+                            } else {
+                                $value = 2; // Non.
+                            }
+                            break;
+                        case 'information':
+                            if ($course->information === null) {
+                                $course->information = '';
+                            }
+
+                            if ($course->informationformat === null) {
+                                $course->informationformat = FORMAT_HTML;
+                            }
+                            $value = ['text' => $course->information, 'format' => $course->informationformat];
+                            $fieldnameform .= '_editor';
+                            break;
+                        case 'show_policy':
+                            $value = $course->showpolicy;
+                            if ($value === null) {
+                                $value = 0;
+                            }
+                            break;
+                    }
+
+                    if ($value === null) {
+                        continue;
+                    }
+
+                    $mformdata = (object) [$fieldnameform => $value];
+                    $data->instance_form_save($mformdata);
+                }
+            }
+
+            // Supprime la table apsolu_courses.
+            $dbman->drop_table($table);
+        }
+
+        $table = new xmldb_table('apsolu_complements');
+        if ($dbman->table_exists($table) === true) {
+            $complements = $DB->get_records('apsolu_complements', ['federation' => 0]);
+            if (count($complements) > 0) {
+                // Initialise un type de format "cours".
+                $record = new stdClass();
+                $record->shortname = 'autonome';
+                $record->name = 'Pratique autonome';
+                // Modèle du libellé de cours: [activité].
+                $record->fullnametemplate = sprintf('%%%02d', $fields['category']->get('id'));
+                $record->color = '#f66151';
+                $record->sortorder = 2;
+                if ($DB->get_record('apsolu_courses_types', ['shortname' => $record->shortname]) === false) {
+                    $DB->insert_record('apsolu_courses_types', $record);
+                }
+
+                // Associe les champs personnalisés au type de format "Pratique autonome".
+                $params = ['name' => 'APSOLU', 'component' => 'core_course', 'area' => 'course', 'itemid' => 0];
+                $category = $DB->get_record('customfield_category', $params, '*', MUST_EXIST);
+                $coursetype = $DB->get_record('apsolu_courses_types', ['shortname' => 'autonome'], '*', MUST_EXIST);
+
+                $coursefields = [
+                    'type' => ['showinadministration' => 1, 'showonpublicpages' => 1],
+                    'category' => ['showinadministration' => 1, 'showonpublicpages' => 1],
+                ];
+
+                foreach ($fields as $field) {
+                    if (isset($coursefields[$field->get('shortname')]) === false) {
+                        continue;
+                    }
+
+                    $record = new stdClass();
+                    $record->coursetypeid = $coursetype->id;
+                    $record->customfieldid = $field->get('id');
+                    $record->showinadministration = $coursefields[$field->get('shortname')]['showinadministration'];
+                    $record->showonpublicpages = $coursefields[$field->get('shortname')]['showonpublicpages'];
+
+                    $conditions = ['coursetypeid' => $record->coursetypeid, 'customfieldid' => $record->customfieldid];
+                    if ($DB->get_record('apsolu_courses_fields', $conditions) !== false) {
+                        continue;
+                    }
+                    $DB->insert_record('apsolu_courses_fields', $record);
+                }
+
+                // Migre l'ancienne table mdl_apsolu_complements.
+                foreach ($complements as $complement) {
+                    $course = $DB->get_record('course', ['id' => $complement->id]);
+                    if ($course === false) {
+                        continue;
+                    }
+
+                    $coursecontext = context_course::instance($course->id);
+                    foreach ($customcategory->get_fields() as $field) {
+                        $shortname = $field->get('shortname');
+                        $fieldnameform = 'customfield_' . $shortname;
+
+                        $params = (object) ['instanceid' => $course->id, 'contextid' => $coursecontext->id];
+                        $data = data_controller::create(0, $params, $field);
+
+                        $value = null;
+                        switch ($shortname) {
+                            case 'type':
+                                $value = $coursetype->id;
+                                break;
+                            case 'category':
+                                $category = $DB->get_record('apsolu_courses_categories', ['id' => $course->category]);
+                                if ($category === false) {
+                                    $category = current($DB->get_records('apsolu_courses_categories'));
+                                    if (empty($category) === false) {
+                                        // Met à jour la catégorie du cours pour une catégorie d'activités APSOLU.
+                                        $course->category = $category->id;
+                                        $DB->update_record('course', $course);
+
+                                        $value = ['categoryid' => $category->id, 'additionalstr' => trim($course->fullname)];
+                                    }
+                                } else {
+                                    $value = ['categoryid' => $course->category, 'additionalstr' => ''];
+                                }
+                                break;
+                        }
+
+                        if ($value === null) {
+                            continue;
+                        }
+
+                        $mformdata = (object) [$fieldnameform => $value];
+                        $data->instance_form_save($mformdata);
+                    }
+                }
+            }
+
+            // Supprime la table apsolu_complements.
+            $dbman->drop_table($table);
+        }
     }
 
     return true;
--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version = 2026061000;
+$plugin->version = 2026083100;
 
-$plugin->release = '5.0.8';
+$plugin->release = '5.2.2';
 
-// Requires Moodle 5.0.0.
-$plugin->requires = 2025041400;
+// Requires Moodle 5.2.0.
+$plugin->requires = 2026042000;
 
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'local_apsolu';
@@ -38,8 +38,8 @@ $plugin->component = 'local_apsolu';
 // The plugin is a stable version.
 $plugin->maturity = MATURITY_STABLE;
 
-// Support Moodle from 5.0.0 to 5.0.x.
-$plugin->supported = [500, 500];
+// Support Moodle from 5.2.0 to 5.2.x.
+$plugin->supported = [502, 502];
 
 // Dependencies on another plugin.
 $plugin->dependencies = [
