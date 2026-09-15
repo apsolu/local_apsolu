5db9ff98916c74d5e6357294e9adca8d7210aaff
Julien Boulen <julien.boulen@univ-rennes2.fr>
2026-09-15 17:24:05 +0200

    fix(payment): modifie le code sur la page des paiements pour éviter l'erreur Paybox 'paiement dupliqué'

--- payment/validation.php
+++ payment/validation.php
@@ -134,16 +134,34 @@ foreach ($paymentcenters as $centerid => $paymentcenter) {
     if ($paymentcenter->due_amount > 0) {
         $transaction = $DB->start_delegated_transaction();
 
-        if (true) {
+        if (empty($enableatouts) === true) {
+            // Traite le cas général, lorsqu'Atouts Normandie n'est pas activé.
+            $payment = new stdClass();
+            $payment->method = 'paybox';
+            $payment->source = 'apsolu';
+            $payment->amount = $paymentcenter->due_amount;
+            $payment->status = Payment::DUE; // Pas payé !
+            $payment->timepaid = null;
+            $payment->timecreated = core_date::strftime('%FT%T');
+            $payment->timemodified = $payment->timecreated;
+            $payment->userid = $USER->id;
+            $payment->paymentcenterid = $paymentcenter->id;
+            $payment->id = $DB->insert_record('apsolu_payments', $payment);
+            $payment->prefix = $paymentcenter->prefix;
+            $payment->codes = [];
+
+            $atoutdeduction = 0;
+        } else {
             // Atouts Normandie est activé.
             // On récupère ou on crée l'enregistrement de paiement principal (mdl_apsolu_payments).
-            $payment = $DB->get_record('apsolu_payments', [
+            $payments = $DB->get_records('apsolu_payments', [
                 'userid' => $USER->id,
                 'paymentcenterid' => $paymentcenter->id,
-                'status' => 0,
-            ], '*', IGNORE_MULTIPLE);
+                'status' => Payment::DUE,
+            ], $sort = 'timecreated DESC');
 
-            if (!$payment) {
+            $regenid = false;
+            if (count($payments) === 0) {
                 // Création si inexistant.
                 $payment = new \stdClass();
                 $payment->method = 'paybox';
@@ -156,6 +174,8 @@ foreach ($paymentcenters as $centerid => $paymentcenter) {
                 $payment->paymentcenterid = $paymentcenter->id;
                 $payment->id = $DB->insert_record('apsolu_payments', $payment);
             } else {
+                $payment = current($payments);
+
                 // Mise à jour du montant si le panier a changé.
                 $payment->amount = $paymentcenter->due_amount;
                 $payment->timemodified = core_date::strftime('%FT%T');
@@ -163,6 +183,8 @@ foreach ($paymentcenters as $centerid => $paymentcenter) {
 
                 // Nettoyage des anciens items pour les recréer proprement.
                 $DB->delete_records('apsolu_payments_items', ['paymentid' => $payment->id]);
+
+                $regenid = true;
             }
 
             // GESTION ATOUTS NORMANDIE.
@@ -172,6 +194,14 @@ foreach ($paymentcenters as $centerid => $paymentcenter) {
             $atoutdeduction = 0;
             if ($atoutrecord) {
                 $atoutdeduction = $atoutrecord->amount;
+            } else if ($regenid === true) {
+                // L'utilisateur n'utilise pas Atouts Normandie. On génère un nouvel ID pour la transaction de paiement, afin
+                // d'éviter l'erreur "paiement dupliqué".
+                unset($payment->id);
+
+                $payment->timecreated = core_date::strftime('%FT%T');
+                $payment->timemodified = $payment->timecreated;
+                $payment->id = $DB->insert_record('apsolu_payments', $payment);
             }
         }
 
